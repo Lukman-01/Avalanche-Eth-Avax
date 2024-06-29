679215cdd99da2098cf005f27fdebf0ba7eb993b
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-06-29 06:46:31 +0100

    contract

---
+++ Create-and-Mint-Token/contracts/NewToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NewToken is ERC20, Ownable {
+    constructor()
+        ERC20("NewToken", "NTK")
+        Ownable(msg.sender)
+    {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+}
